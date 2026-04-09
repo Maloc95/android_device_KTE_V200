ca8979424a484f2a0673a746005e0f6cc9988c23
Maloc95 <263300646+Maloc95@users.noreply.github.com>
2026-04-09 16:29:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,16 +6,3 @@
 #
 
 LOCAL_PATH := device/kte/V200
-
-# 添加以下 FDE 解密所需的底层库复制规则
-# 复制解密服务 Binaries
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:recovery/root/vendor/bin/hw/android.hardware.gatekeeper@1.0-service \
-    $(LOCAL_PATH)/recovery/root/vendor/bin/hw/android.hardware.keymaster@3.0-service:recovery/root/vendor/bin/hw/android.hardware.keymaster@3.0-service
-
-# 复制解密核心库 Libraries
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/hw/android.hardware.gatekeeper@1.0-impl.so:recovery/root/vendor/lib/hw/android.hardware.gatekeeper@1.0-impl.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/hw/android.hardware.keymaster@3.0-impl.so:recovery/root/vendor/lib/hw/android.hardware.keymaster@3.0-impl.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/hw/gatekeeper.default.so:recovery/root/vendor/lib/hw/gatekeeper.default.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/hw/libSoftGatekeeper.so:recovery/root/vendor/lib/hw/libSoftGatekeeper.so
